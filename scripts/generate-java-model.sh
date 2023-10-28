e48530d768b35602a6ac62e581dc33f659017bf1
Qusai Fares <qusaifares@gmail.com>
2023-10-27 18:21:30 -0700

    Add active game

--- scripts/generate-java-model.sh
+++ scripts/generate-java-model.sh
@@ -1,59 +1,43 @@
 #!/bin/sh
 
-# Paths for the spec and the output directory
-SPEC_PATH="./services/leaderboard-api/v1/openapi.yaml"
-OUTPUT_PATH="./output/model/java"
+# Array of OpenAPI spec file paths
+SPEC_PATHS=(
+    "./services/leaderboard-api/v1/openapi.yaml"
+    "./services/active-game-api/v1/openapi.yaml"
+    # Add more spec paths here
+)
 
-echo "Generating Java SDK..."
+# Output directory base path
+OUTPUT_BASE_PATH="./output/model/java"
 
-# This command assumes you have the OpenAPI Generator CLI installed. 
-# If not, you can use a Docker image or a local installation.
-openapi-generator-cli generate \
--i $SPEC_PATH \
--g java \
--o $OUTPUT_PATH \
---additional-properties apis=None \
---additional-properties supportingFiles=None \
---additional-properties modelDocs=false \
---additional-properties modelTests=false \
---additional-properties modelPackage=com.bakeryleaderboard.service.servicemodel.model
+echo "Generating Java SDKs..."
 
+# Loop through each spec file
+for SPEC_PATH in "${SPEC_PATHS[@]}"
+do
+    # Generate output path based on spec file name
+    SPEC_NAME=$(basename "$SPEC_PATH" .yaml)
+    OUTPUT_PATH="$OUTPUT_BASE_PATH/$SPEC_NAME"
 
-if [ $? -eq 0 ]; then
-    echo "Java SDK generated successfully!"
-else
-    echo "Error generating Java SDK."
-fi
+    echo "Generating Java SDK for $SPEC_NAME..."
 
+    # Generate Java SDK
+    openapi-generator-cli generate \
+    -i "$SPEC_PATH" \
+    -g java \
+    -o "$OUTPUT_PATH" \
+    --additional-properties apis=None \
+    --additional-properties supportingFiles=None \
+    --additional-properties modelDocs=false \
+    --additional-properties modelTests=false \
+    --additional-properties modelPackage=com.bakeryleaderboard.service.servicemodel.model
 
-# # Paths for the spec and the output directory
-# SPEC_PATH="./services/leaderboard-api/v1/openapi.yaml"
-# OUTPUT_PATH="./output/model/java"
+    # Check if the generation was successful
+    if [ $? -eq 0 ]; then
+        echo "Java SDK for $SPEC_NAME generated successfully!"
+    else
+        echo "Error generating Java SDK for $SPEC_NAME."
+    fi
+done
 
-# echo "Generating Java SDK..."
-
-# # This command assumes you have the OpenAPI Generator CLI installed. 
-# # If not, you can use a Docker image or a local installation.
-# openapi-generator-cli generate \
-# -i $SPEC_PATH \
-# -g java \
-# -o $OUTPUT_PATH \
-# --additional-properties apis=None \
-# --additional-properties supportingFiles=None \
-# --additional-properties modelDocs=false \
-# --additional-properties modelTests=false \
-# --additional-properties modelPackage=com.bakeryleaderboard.service.servicemodel.model \
-# --additional-properties withXml=false \
-# --additional-properties dateLibrary=java8 \
-# --additional-properties hideGenerationTimestamp=true \
-# --additional-properties serializableModel=true \
-# --additional-properties useBeanValidation=true \
-# --additional-properties toModelNameSuffix=Model \
-# --additional-properties toModelNamePrefix="" \
-# --additional-properties generateBuilders=true
-
-# if [ $? -eq 0 ]; then
-#     echo "Java SDK generated successfully!"
-# else
-#     echo "Error generating Java SDK."
-# fi
+echo "Java SDK generation process completed."
--- scripts/generate-ts-sdk.js
+++ scripts/generate-ts-sdk.js
@@ -2,17 +2,19 @@
 
 const { execSync } = require('child_process');
 
-const SPEC_PATH = './services/leaderboard-api/v1/openapi.yaml';
+const SPEC_PATHS = ['./services/leaderboard-api/v1/openapi.yaml', './services/active-game-api/v1/openapi.yaml'];
 const OUTPUT_PATH = './output/model/typescript';
 
 try {
     console.log('Generating TypeScript SDK...');
 
-    // This command assumes you have @openapitools/openapi-generator-cli globally installed.
-    // Alternatively, you can use a local installation or even a Docker image.
-    execSync(`openapi-generator-cli generate -i ${SPEC_PATH} -g typescript-fetch -o ${OUTPUT_PATH}`);
-
-    console.log('TypeScript SDK generated successfully!');
+    SPEC_PATHS.forEach((path) => {
+        // This command assumes you have @openapitools/openapi-generator-cli globally installed.
+        // Alternatively, you can use a local installation or even a Docker image.
+        execSync(`openapi-generator-cli generate -i ${path} -g typescript-fetch -o ${OUTPUT_PATH}`);
+        
+        console.log('TypeScript SDK generated successfully!');
+    })
 } catch (error) {
     console.error('Error generating TypeScript SDK:', error);
 }
