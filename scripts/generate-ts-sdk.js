6a5b7fd1e95de8b7543daa8920ead9075e86942e
Qusai Fares <qusaifares@gmail.com>
2023-10-29 01:51:34 -0700

    Separate api directories

--- scripts/generate-ts-sdk.js
+++ scripts/generate-ts-sdk.js
@@ -2,16 +2,18 @@
 
 const { execSync } = require('child_process');
 
-const SPEC_PATHS = ['./services/leaderboard-api/v1/openapi.yaml', './services/active-game-api/v1/openapi.yaml'];
-const OUTPUT_PATH = './output/model/typescript';
+const SPEC_PATHS = [
+    { input: './services/leaderboard-api/v1/openapi.yaml', output: './output/model/typescript/leaderboard' },
+    { input: './services/active-game-api/v1/openapi.yaml', output: './output/model/typescript/active-game' },
+]
 
 try {
     console.log('Generating TypeScript SDK...');
 
-    SPEC_PATHS.forEach((path) => {
+    SPEC_PATHS.forEach(({input, output}) => {
         // This command assumes you have @openapitools/openapi-generator-cli globally installed.
         // Alternatively, you can use a local installation or even a Docker image.
-        execSync(`openapi-generator-cli generate -i ${path} -g typescript-fetch -o ${OUTPUT_PATH}`);
+        execSync(`openapi-generator-cli generate -i ${input} -g typescript-fetch -o ${output}`);
         
         console.log('TypeScript SDK generated successfully!');
     })
